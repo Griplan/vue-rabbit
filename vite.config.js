a2498f260e41ffd83e9133b66a921fb920471f55
Griplan <2424015851@qq.com>
2024-09-28 12:40:53 +0800

    定制主体色

--- vite.config.js
+++ vite.config.js
@@ -14,15 +14,26 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     AutoImport({
+      
       resolvers: [ElementPlusResolver()],
-    }),
+    }),// 1. 配置elementPlus采用sass样式配色系统
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))//实际转化路径
     }
-  }
+  },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // 2. 自动导入定制化样式文件进行样式覆盖
+                additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+            }
+        }
+    }
 })
