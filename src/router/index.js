3c27e765dba2e8dbcbc439c29bd080798b389316
Griplan <2424015851@qq.com>
2024-09-29 10:21:49 +0800

    新增二级路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +15,17 @@ const router = createRouter({
     },
     {
       path: '/',
-      component:Layout
+      component: Layout,
+      children: [
+        {
+          path: '',
+          component:Home
+        },
+        {
+          path: 'category',
+          component:Category
+        }
+      ]
     }
   ]
 })
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,4 +1,10 @@
-<template>layout</template>
+<template>
+  <div>
+    我是首页layoutindex
+    <!-- 二级路由出口 -->
+    <RouterView></RouterView>
+  </div>
+</template>
 
 <script></script>
 
