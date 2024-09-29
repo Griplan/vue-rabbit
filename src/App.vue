ba8871ab4b2b1471ecdc68127552227f36b54c86
Griplan <2424015851@qq.com>
2024-09-29 11:08:01 +0800

    在vite.config.js中配置定制化样式文件的自动导入，之后可以直接用共享css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <RouterView></RouterView>
+  <div class="test">试一下var.scss能否正常工作</div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+</style>
