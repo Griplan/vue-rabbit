81dc3425a400636fbafa612793de40597d406caf
Griplan <2424015851@qq.com>
2024-10-10 16:35:57 +0800

    完成轮播图

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,13 +1,21 @@
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { ref } from "vue";
+import { getBannerAPI } from "@/apis/layout";
+
+onMounted(() => getBanner()); //挂载时就调用
+
+const bannerList = ref([]);
+async function getBanner() {
+  bannerList.value = (await getBannerAPI()).data.result;
+}
+</script>
 
 <template>
   <div class="home-banner">
     <el-carousel height="500px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <img
-          src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-04-15/6d202d8e-bb47-4f92-9523-f32ab65754f4.jpg"
-          alt=""
-        />
+      <el-carousel-item v-for="item in bannerList" :key="item">
+        <img :src="item.imgUrl" />
       </el-carousel-item>
     </el-carousel>
   </div>
